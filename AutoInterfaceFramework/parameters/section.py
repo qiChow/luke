d53331cae2c50791c98d107ac41cd68096b2d70a
Qi zhou <qi.zhou@shopee.com>
2018-09-11 20:30:11 +0800

    delete old one

--- AutoInterfaceFramework/parameters/section.py
+++
@@ -1,9 +0,0 @@
-'''
-按照ini的分组命名参数文件，继承common_config类
-'''
-
-from parameters.common_config import CommonConfig
-
-class Section(CommonConfig):
-    def key(self):
-        return self.cf.get("TestSuitName", "key")
